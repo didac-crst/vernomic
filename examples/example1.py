c5d0c5747fdc295ababc574351541ac2eca2db12
Didac Cristobal <didac.crst@gmail.com>
2025-07-28 16:32:46 +0200

    Update examples

--- examples/example1.py
+++ examples/example1.py
@@ -7,8 +7,12 @@ import random
 from vernomic import Vernomic
 
 if __name__ == "__main__":
+    file_extension = ".h5"
+    root_name = "model"
+    suffix_name = "v1"
     start = datetime(2023, 12, 20)  # Start date and time
     for i in range(20):
         dt = start + timedelta(days=i) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
-        vernomic = Vernomic(root_name="model", date=dt)
+        vernomic = Vernomic(root_name=root_name, suffix_name=suffix_name, file_extension=file_extension, date=dt)
         print(f"Date: {dt.date()} -> {vernomic}")
+        vernomic.to_yaml("../test_output/")
--- examples/example2.py
+++ examples/example2.py
@@ -8,8 +8,12 @@ from vernomic import Vernomic
 
 if __name__ == "__main__":
     # Generate 20 random dates starting from a specific date
+    file_extension = ".h5"
+    root_name = "model"
+    suffix_name = "v1"
     start = datetime(2024, 1, 1)  # Start date and time
     for i in range(20):
         dt = start + timedelta(days=random.randint(0, 1000)) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
-        vernomic = Vernomic(root_name="model", date=dt)
+        vernomic = Vernomic(root_name=root_name, suffix_name=suffix_name, file_extension=file_extension, date=dt)
         print(f"Date: {dt.date()} -> {vernomic}")
+        vernomic.to_yaml("../test_output/")

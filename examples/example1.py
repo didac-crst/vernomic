ff22be9d01b83d1f3865556a794463ab0a530c95
Didac Cristobal <didac.crst@gmail.com>
2025-07-29 08:52:32 +0200

    Adapt examples according to lib

--- examples/example1.py
+++ examples/example1.py
@@ -10,10 +10,14 @@ if __name__ == "__main__":
     file_extension = ".h5"
     root_name = "model"
     suffix_name = "v1"
-    start = datetime(2023, 12, 20)  # Start date and time
-    for i in range(20):
+    start = datetime(2024, 12, 25)  # Start date and time
+    for i in range(14):
+        # Generate a sequential random date and time
+        dt = start + timedelta(days=i) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
         add_description = random.choice([True, False])
         add_suffix = random.choice([True, False])
+        display_version_time = random.choice([True, False])
+        divide_char = "_"
         if add_description:
             description = f"Example description {i + 1}"
         else:
@@ -22,8 +26,12 @@ if __name__ == "__main__":
             suffix_name = f"v{random.randint(1, 10)}"
         else:
             suffix_name = None
-        # Generate a random date and time
-        dt = start + timedelta(days=i) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
-        vernomic = Vernomic(root_name=root_name, suffix_name=suffix_name, file_extension=file_extension, date=dt, description=description)
+        vernomic = Vernomic(root_name=root_name,
+                            suffix_name=suffix_name,
+                            file_extension=file_extension,
+                            display_version_time=display_version_time,
+                            date=dt,
+                            description=description,
+                            divide_char=divide_char)
         print(f"Date: {dt.date()} -> {vernomic}")
         vernomic.to_yaml("../test_output/")
--- examples/example2.py
+++ examples/example2.py
@@ -10,10 +10,14 @@ if __name__ == "__main__":
     # Generate 20 random dates starting from a specific date
     file_extension = ".h5"
     root_name = "model"
-    start = datetime(2024, 1, 1)  # Start date and time
+    start = datetime(2020, 1, 1)  # Start date and time
     for i in range(20):
+        # Generate a random date and time
+        dt = start + timedelta(days=random.randint(0, 10000)) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
         add_description = random.choice([True, False])
         add_suffix = random.choice([True, False])
+        display_version_time = random.choice([True, False])
+        divide_char = random.choice(["_", "-"])
         if add_description:
             description = f"Example description {i + 1}"
         else:
@@ -22,8 +26,12 @@ if __name__ == "__main__":
             suffix_name = f"v{random.randint(1, 10)}"
         else:
             suffix_name = None
-        # Generate a random date and time
-        dt = start + timedelta(days=random.randint(0, 1000)) + timedelta(hours=random.randint(0, 23), minutes=random.randint(0, 59))  # Add time to the date
-        vernomic = Vernomic(root_name=root_name, suffix_name=suffix_name, file_extension=file_extension, date=dt, description=description)
+        vernomic = Vernomic(root_name=root_name,
+                            suffix_name=suffix_name,
+                            file_extension=file_extension,
+                            display_version_time=display_version_time,
+                            date=dt,
+                            description=description,
+                            divide_char=divide_char)
         print(f"Date: {dt.date()} -> {vernomic}")
         vernomic.to_yaml("../test_output/")
